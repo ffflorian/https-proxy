b503b727d41b84191e1c6860dc4857c70fda05e9
Florian Keller <github@floriankeller.de>
2019-09-06 13:47:40 +0200

    fix: Read correct package.json

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
 import * as program from 'commander';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import {HttpsProxy} from './HttpsProxy';
 
-const {description, name, version} = require('../package.json');
+const defaultPackageJsonPath = path.join(__dirname, 'package.json');
+const packageJsonPath = fs.existsSync(defaultPackageJsonPath)
+  ? defaultPackageJsonPath
+  : path.join(__dirname, '../package.json');
+
+const packageJson = fs.readFileSync(packageJsonPath, 'utf-8');
+const {description, name, version}: {description: string; name: string; version: string} = JSON.parse(packageJson);
 
 program
   .name(name.replace(/^@[^/]+\//, ''))
